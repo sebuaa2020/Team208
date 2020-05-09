a858e6a453642339c6cd877ef571f142e59ed86b
Yzx835 <yzx835@buaa.edu.cn>
2020-05-09 19:50:53 +0800

    update robot avoidance

--- code/src/robot2077_basic/src/robot_move.cpp
+++ code/src/robot2077_basic/src/robot_move.cpp
@@ -1,12 +1,13 @@
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
 #include "robot2077_basic/Movemsg.h"
+#include "robot2077_basic/IsAvoidance.h"
 #include <iostream>
 
 ros::Publisher vel_pub;
 geometry_msgs::Twist vel;
-geometry_msgs::Twist vel_zero;
-bool avoidence_flag = false;
+
+ros::ServiceClient checkAvoidance;
 
 void move_listen(const robot2077_basic::Movemsg::ConstPtr& msg) {
     ROS_INFO("x: %.6f, y: %.6f, z: %.6f", msg->x, msg->y, msg->z);
@@ -16,22 +17,25 @@ void move_listen(const robot2077_basic::Movemsg::ConstPtr& msg) {
     vel.angular.x = 0;
     vel.angular.y = 0;
     vel.angular.z = msg->z;
-    if (!avoidence_flag) vel_pub.publish(vel);
-    else vel_pub.publish(vel_zero);
+
+    robot2077_basic::IsAvoidance srv;
+    srv.request.x = msg->x;
+    srv.request.y = msg->y;
+    if (!checkAvoidance.call(srv) || !srv.response.isavoidance) vel_pub.publish(vel);
+    else {
+        vel.linear.x = 0;
+        vel.linear.y = 0;
+        vel_pub.publish(vel);
+    }
 }
 
 int main(int argc, char **argv) {
 
     ros::init(argc, argv, "robot_move");
     ros::NodeHandle n;
-    vel_zero.linear.x = 0;
-    vel_zero.linear.y = 0;
-    vel_zero.linear.z = 0;
-    vel_zero.angular.x = 0;
-    vel_zero.angular.y = 0;
-    vel_zero.angular.z = 0;
     
     vel_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
+    checkAvoidance = n.serviceClient<robot2077_basic::IsAvoidance>("/robot2077/robot_move/is_avoidance");
 
     ros::Subscriber sub = n.subscribe("/robot2077/robot_move/vel", 10, move_listen);
     ros::spin();
