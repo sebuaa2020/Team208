f2d8c386539fd25a1347e8b423f1a330a35e8c73
fjhzhixi <18810827418@163.com>
2020-05-10 15:42:25 +0800

    change floder list

---
+++ code/build/robot2077_navigation/catkin_generated/pkg.installspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
+PROJECT_CATKIN_DEPENDS = "urdf;xacro".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "robot2077_navigation"
+PROJECT_SPACE_DIR = "/home/fjh/Team208/code/install"
+PROJECT_VERSION = "0.0.1"
---
+++ code/build/vel_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/fjh/Team208/code/src/vel_pkg
+# Build directory: /home/fjh/Team208/code/build/vel_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
