7ea80ee8dba87aea08d4f5a6ffa4f9c590a8cce7
fjhzhixi <18810827418@163.com>
2020-05-10 13:54:17 +0800

    delete add file

--- demo_ws/build/robot_sim_demo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fjh/Team208/demo_ws/src/robot_sim_demo
-# Build directory: /home/fjh/Team208/demo_ws/build/robot_sim_demo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- demo_ws/build/vel_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fjh/Team208/demo_ws/src/vel_pkg
-# Build directory: /home/fjh/Team208/demo_ws/build/vel_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
